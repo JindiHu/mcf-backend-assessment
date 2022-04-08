6692f20b456a86e64dfe0423e1f8846812f57ab0
Elson Lim <elsonlim7@gmail.com>
2022-04-08 18:51:47 +0800

    Add route and failing test for cities route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cities = require("../data/cities");
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.get("/api/v1/cities", (req, res) => {
+  res.json({});
 });
 
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("./app");
+const cities = require("../data/cities");
 
 describe("app", () => {
   it("should return hello world", async () => {
@@ -7,5 +8,14 @@ describe("app", () => {
 
     expect(response.status).toEqual(200);
     expect(response.text).toEqual("hello world");
+  });
+
+  describe("GET /api/v1/cities", () => {
+    it("should return data from cities", async () => {
+      const response = await request(app).get("/api/v1/cities");
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(cities);
+    });
   });
 });
