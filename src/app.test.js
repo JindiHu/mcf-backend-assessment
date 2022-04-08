fcabc377b57bb33ff5e67f2bb265d04c207f52cd
Elson Lim <elsonlim7@gmail.com>
2022-04-08 18:40:13 +0800

    Set up supertest on root route

---
+++ src/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("app", () => {
+  it("should return hello world", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toEqual(200);
+    expect(response.text).toEqual("hello world");
+  });
+});
